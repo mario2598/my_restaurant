c11a5f5ed6a389500d896f01738031f38633be01
mario2598 <43936591+mario2598@users.noreply.github.com>
2023-10-29 23:51:50 -0600

    Update SpaceUtil.php

--- app/Traits/SpaceUtil.php
+++ app/Traits/SpaceUtil.php
@@ -701,7 +701,7 @@ trait SpaceUtil
       $carbonDate->setLocale('es');
   
       // Formatea la fecha en español
-      $fechaAux = $carbonDate->isoFormat('Ddd, D [de] MMMM - h:i A');
+      $fechaAux = $carbonDate->isoFormat('ddd, D [de] MMMM - h:i A');
       $fechaAux = ucfirst($fechaAux);
       return $fechaAux;
   }
