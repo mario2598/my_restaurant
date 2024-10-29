93dd7179618f4464a9307bf4b9a2ff4be318e7da
root <mario.flores2598@gmail.com>
2024-10-29 23:54:01 +0000

    Corrige errores en la ruta de facturación y actualiza archivo .env

--- storage/framework/views/6c3945dc08ebe6015d70f0a65bc97a1db1cbae5c.php
+++
@@ -1,7 +0,0 @@
-
-<footer class="main-footer">
-  <div class="footer-left">
-    Coffee To Go | The coffee experience
-  </div>
-  
-</footer><?php /**PATH /var/www/CoffeeToGo/resources/views/layout/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/9ba8855c578c2ad15d021656a499cec84af76b53.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('content'); ?>  
-
-<?php echo $__env->make('layout.sidebar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
-<?php echo $__env->make('main.inicio', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php $__env->stopSection(); ?>
-<?php echo $__env->make('layout.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/CoffeeToGo/resources/views/inicio.blade.php ENDPATH**/ ?>
